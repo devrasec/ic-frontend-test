0e60b5e8a47ff65835a657910eb67e1b922728a7
Luis Contreras <one.tmac@gmail.com>
2018-12-23 19:23:03 -0400

    feat: Install and setup styled-components, add global styles

---
+++ src/components/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function App() {
+  return <h1>Instacarro Frontend Test</h1>;
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createGlobalStyle } from 'styled-components';
+import { Normalize } from 'styled-normalize';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Open Sans', Helvetica, sans-serif;
+  }
+`;
+
+function Root() {
+  return (
+    <React.Fragment>
+      <Normalize />
+      <GlobalStyle />
+      <App />
+    </React.Fragment>
+  );
+}
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
